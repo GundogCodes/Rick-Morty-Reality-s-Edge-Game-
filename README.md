6d333d1429ba1f5a98abfc3fe51320c1b2c2e555
Gunish <gunishsharma20@gmail.com>
2023-04-28 03:27:05 -0600

    ADDED IMG AND POSITIONED learn about CSS Animation also this is not pyGame

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project_1
 //Realitys Edge//
+ <img  id ='player' src = "https://www.freepnglogos.com/uploads/mario-png/super-mario-bros-clip-art-fiesta-for-geeks-0.png" width="100px" height="150px"  >
--- script/main.js
+++ script/main.js
@@ -1,1 +1,30 @@
-console.log('works')
+
+console.log('work2!!!')
+class Player{
+    constructor(name){
+
+    }
+}
+
+class Enemy{
+    constructor(){
+
+    }
+}
+/*----- constants -----*/
+/*----- state variables -----*/
+let gameOn =true
+/*----- cached elements  -----*/
+const keyBoard = document.getElementsByClassName('keyboard')
+const player = document.getElementById('player')
+/*----- event listeners -----*/
+player.addEventListener('click',handleClick)
+
+/*----- functions -----*/
+let x = 300
+function handleClick(){
+    
+  
+
+}
+
