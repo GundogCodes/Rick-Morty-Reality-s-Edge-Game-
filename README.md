c404c61db63610dae94a7c4aa14d8c381cc497ad
Gunish <gunishsharma20@gmail.com>
2023-04-30 01:34:36 -0600

    fixed positions and jump method added enemy move method

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # project_1
 //Realitys Edge//
  <img  id ='player' src = "https://www.freepnglogos.com/uploads/mario-png/super-mario-bros-clip-art-fiesta-for-geeks-0.png" width="100px" height="150px"  >
+ //https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-rick-flying-transparent-png-stickpng-15.png
+//https://png2.cleanpng.com/sh/a2def7cc2c080fa4f903558ea2e126cd/L0KzQYm3V8IxN6d4f5H0aYP2gLBuTf1weqVAReV2aYTrPbTvggJia6Vqip9sYYL3f7F1TfZidl5miuY2dnnyfLr1TgJqa5wyedDtLX3ygsXCTcVjPWI8UaY9ZUO3SYW3TsI0PGM3T6g5MUW2QoG9VsEyPmo4SZD5bne=/kisspng-morty-smith-character-cartoon-fan-art-violin-rick-and-morty-5b517944e34940.234227601532066116931.png
+
+
+
+
 
  If playerHealth is 1 or more,
     create Enemy's and randomize enemyPositions across the right of screen
--- script/main.js
+++ script/main.js
@@ -1,49 +1,65 @@
 
 console.log('works!!!')
 class Player{
+
     constructor(){
         this.element = document.getElementById('player')
+        this.element.style.bottom = '1010px'
+        this.element.style.right = '800px'
+        
+    }
     
+    jump() {
     
+        console.log('playerCLICKED!')
+        this.element.classList.toggle('jump')
+        
     }
 
+    getEl(){
+        return this.element
+    }
+    
     
 }
 
 class Enemy{
     constructor(){
-
+        this.element = document.getElementById('enemy')
+        this.element.style.bottom = '1260px'
+        this.element.style.right = '70px'
     }
+
+    move() {
+    this.element.classList.add('move')
+
+    }  
 }
 
 
 /*----- constants -----*/
 
-const player =document.getElementById('player')
-player.addEventListener('click',player.jump)
-console.log('playerr',player)
 
+const player = new Player()
+const enemy = new Enemy()
+ 
 /*----- state variables -----*/
 
 let gameOn =true
-
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
+
 document.body.addEventListener('keydown', function(e){
     console.log(e)
-if(e.key === ' '){jump()} 
-})
+if(e.key === ' '){player.jump()} 
+}) 
+
+player.getEl().addEventListener('click',function(){player.jump()})
+
 
 
 /*----- functions -----*/
 
-function jump() {
-    
-    console.log('playerCLICKED!')
-    player.classList.toggle('jump')
-
-      
-    
-}
+enemy.move()
